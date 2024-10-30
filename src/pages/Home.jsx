83a078634f3f94afae67dac0206374d7a86b53ee
thales0591 <thales0591@gmail.com>
2024-10-30 11:11:45 -0300

    :sparkles: feat: create a context changer component and import context resources into the pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
+import { useContext } from "react"
+import { CounterContext } from "../context/CounterContext"
+import ChangeCounter from "../components/ChangeCounter"
+
 const Home = () => {
+
+  const {counter} = useContext(CounterContext)
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      <p>Valor do contador: {counter}</p>
+      <ChangeCounter/>
+    </div>
   )
 }
 
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,15 @@
+import { useContext } from "react"
+import { CounterContext } from "../context/CounterContext"
+
 const Products = () => {
+
+  const {counter} = useContext(CounterContext)
+  
   return (
-    <div>Products</div>
+    <div>
+      <h1>Products</h1>
+      <p>Valor do contador: {counter}</p>
+    </div>
   )
 }
 
