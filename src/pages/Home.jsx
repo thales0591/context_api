bffd4250757f5a5dade68c9da80c79154de337ab
thales0591 <thales0591@gmail.com>
2024-10-30 11:33:01 -0300

    :recycle: refactor: create a context hook to avoid unnecessary imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,18 @@
-import { useContext } from "react"
-import { CounterContext } from "../context/CounterContext"
-import ChangeCounter from "../components/ChangeCounter"
+// import { useContext } from "react"
+// import { CounterContext } from "../context/CounterContext"
+import ChangeCounter from "../components/ChangeCounter";
+import useCounterContext from "../hooks/useCounterContext";
 
 const Home = () => {
-
-  const {counter} = useContext(CounterContext)
+  // const {counter} = useContext(CounterContext)
+  const { counter } = useCounterContext();
   return (
     <div>
       <h1>Home</h1>
       <p>Valor do contador: {counter}</p>
-      <ChangeCounter/>
+      <ChangeCounter />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,16 +1,14 @@
-import { useContext } from "react"
-import { CounterContext } from "../context/CounterContext"
+import useCounterContext from "../hooks/useCounterContext";
 
 const Products = () => {
+  const { counter } = useCounterContext();
 
-  const {counter} = useContext(CounterContext)
-  
   return (
     <div>
       <h1>Products</h1>
       <p>Valor do contador: {counter}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
